f9e18081a2aae8758f4e040d3d3a700e2f0dfb20
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2024-05-31 13:20:40 +0100

    JSX & Hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-
+import React, { useState } from "react";
+import Welcome from "./Welcome";
 function App() {
   // Initialize  code js
 
@@ -9,11 +10,11 @@ function App() {
   const element = <h1> hello world </h1>;
 
   //object
-  const etudiant = {
-    nom: " Mohamed Aziz",
-    prenom: "Ben Ismail",
-    age: 24,
-  };
+  // const etudiant = {
+  //   nom: " Mohamed Aziz",
+  //   prenom: "Ben Ismail",
+  //   age: 24,
+  // };
 
   //fonction return user
   function getEtudiant(etudiant) {
@@ -26,6 +27,7 @@ function App() {
   }
 
   function getObjectV2(etudiant) {
+    //2
     const results = etudiant.nom + " " + etudiant.prenom + " " + etudiant.age;
     return results;
   }
@@ -38,36 +40,107 @@ function App() {
   ];
 
   const codeCss = {
-    color : "#2ecc71",
-    fontSize :"26px",
-  }
+    color: "#2ecc71",
+    fontSize: "26px",
+  };
 
+  const [count, setCount] = useState(0); //const count = 0;   setCount(1)
+
+  const [state, setState] = useState("a");
+
+  const [{ color, backround }, setData] = useState({
+    color: "red",
+    backround: "purple",
+  });
 
   return (
-    // HTML pro
+    // jsx
     <div className="App">
       <header className="App-header">
         {/* constante */}
-        {name}
+        {/* {name}
         <br></br>
         {age}
         {element}
 
         {/* object */}
         <div className="App-link ">
-          {etudiant.nom} {etudiant.prenom} {etudiant.age}
+          {/* {etudiant.nom} {etudiant.prenom} {etudiant.age} */}
         </div>
 
-        <h1>hello {getEtudiant("Oumaima")} </h1>
+        {/* <h1>hello {getEtudiant("Oumaima")} </h1>
 
         <h2>getObjectV1 : {getObjectV1(etudiant)}</h2>
-        <h2>getObjectV2 : {getObjectV2(etudiant)}</h2>
+        <h2>getObjectV2 : {getObjectV2(etudiant)}</h2> */}
+        {/* 
+        {listeEtudiant.map(function (item) {  //function adiya
+          return (
+            <div key={item.objectID}>
+              {item.nom} {item.prenom} {item.age}
+            </div>
+          );
+        })} */}
 
-        {listeEtudiant.map(function (item) {
-          return <div key={item.objectID} > {item.nom} {item.prenom} {item.age}  </div>;
-        })}
+        {/* {listeEtudiant.map( (item) =>   // function lamda
+          (
+            <div key={item.objectID}>
+              {item.nom} {item.prenom} {item.age}
+            </div>
+          )
+        )} */}
+
+        {/* <table border={1}>
+          <thead>
+            <tr>
+              <td>Nom</td>
+              <td>Prenom</td>
+              <td>Age</td>
+            </tr>
+          </thead>
+          <tbody> */}
+        {/* {listeEtudiant.map(function (item) {
+              return (
+              <tr key={item.objectID}>
+                <td>{item.nom}</td>
+                <td>{item.prenom}</td>
+                <td>{item.age}</td>
+              </tr>
+              )
+            })} */}
+
+        {/* {listeEtudiant.map( (item) => ( 
+            <tr key={item.objectID}>
+              <td>{item.nom}</td>
+              <td>{item.prenom}</td>
+              <td>{item.age}</td>
+            </tr>
+            ))}
+          </tbody>
+        </table> */}
 
         <p className="maClasse"> Formation React </p>
+        {/* 
+        <Welcome name="Dora thoraya" /> */}
+
+        {count}
+        <button onClick={() => setCount(count + 1)}>Cliquez ici</button>
+
+        {state}
+        <button onClick={() => setState(state + "a")}>Ajouter A</button>
+        <br />
+        <input onChange={(e) => setState(e.target.value)} />
+
+        {color}
+        <br />
+        {backround}
+        <br />
+        <input
+          onChange={(e) =>
+            setData( 
+              (current) => ({ ...current, color: e.target.value })
+            )
+          }
+        />
       </header>
     </div>
   );
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Welcome(props) {
+  return (
+    <div>
+      <h1> Bonjour depuis un autre composant {props.name}</h1>
+    </div> 
+  )
+}
+
+
